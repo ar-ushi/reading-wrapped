b1f1d4e7573259e8115ff4d697dfac70ec0e6f8d
Arushi <aggarwalarushi5@gmail.com>
2023-12-27 14:55:07 +0530

    wip - add stepper to basicwrapped

--- client/src/components/BasicWrapped.vue
+++ client/src/components/BasicWrapped.vue
@@ -1,12 +1,6 @@
 <template>
 <div class="container padding-top-1rem" >
-    <v-stepper flat mobile bg-color="transparent" v-model="step">
-        <v-stepper-header>
-            <template v-for="i in items" :key="i">
-            <v-divider v-if="i>1"></v-divider>
-            <v-stepper-item  :value="i" color="primary" :ripple="true"></v-stepper-item>
-            </template>
-        </v-stepper-header>
+    <v-stepper flat mobile bg-color="transparent" :items="items">
         <v-stepper-window>
             <v-stepper-window-item v-for="(phrase, i) in stepperPhrases" :value="i" :key="`${i}--content`">
             <v-card v-html="phrase" color="transparent"></v-card>
@@ -21,10 +15,8 @@ import { ref, toRaw } from 'vue';
 import { useWrappedStore } from '../store/store';
 
     const store = useWrappedStore();
-    const step: number = 1;
-    const items: number = 5;
+    const items: string[] = ['0', '1', '2', '3', '4'];
     const wrappedData = ref(store.getWrappedData);
-    const introText = ref('')
     const username = wrappedData.value.username;
     const totalbooks = wrappedData.value.totalbooksread;
     const totalpages = wrappedData.value.totalpagesread;
@@ -32,13 +24,13 @@ import { useWrappedStore } from '../store/store';
     const daysspentreading = convertMinutesToDays(minutesspentreading);
     const longBookCovers = getLongestBooksCover(4);
     const stepperPhrases = [
-        `Hi ${username}!<h2 class="text-secondary font-size-3rem">Ready to dive into your wrapped?</h2>`,
+    `Hi ${username}!<h2 class="text-secondary font-size-3rem">Ready to dive into your wrapped?</h2>`,
     `2023 was full of highs and lows, but more importantly, it was a year filled with books. <h2 class="text-secondary font-size-3rem">You've read a total of ${ totalbooks} books!</h2>`,
     `You read a total of ${totalpages} pages. According to our calculations, that's a total of <span class="text-secondary">${minutesspentreading} minutes </span> spent across the year.`,
     `<h2 class="text-secondary">If that doesn't already sound like a lot, that comes close to ${daysspentreading} days.</h2> These books probably filled a lot more of your time than you wish to admit 
     <div class='flx-dis'>${longBookCovers.map((cover: any) => `<img src="${cover}" alt="Book Cover" class="book-cover" />`).join('')}</div>` 
     ]
-    
+    console.log(stepperPhrases)
     function convertPagesToMinutes(pg: string){
         //it takes 2 mins on avg to read a page 
         return (parseInt(pg) * 3)
