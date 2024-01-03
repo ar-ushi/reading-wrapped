9be6384d04fcb84ec59afaa48601f866672db679
Arushi <aggarwalarushi5@gmail.com>
2024-01-03 22:52:40 +0530

    fixed server side code

--- server/fetch_book_details.py
+++ server/fetch_book_details.py
@@ -7,8 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from dotenv import load_dotenv
-import os
 
 
 class BookDetails():
@@ -17,59 +15,19 @@ class BookDetails():
         self.year = year
         self.store_book_details = {'totalbooksread': 0, 'totalpagesread': 0 , 'username': '', 'books': []}
         self.rows = []
-        load_dotenv()
-        
-    def login_to_goodreads(self, driver):
-        login_url = 'https://www.goodreads.com/ap/signin?language=en_US&openid.assoc_handle=amzn_goodreads_web_na&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.goodreads.com%2Fap-handler%2Fsign-in&siteState=102cb5ef9d6283aecfd2282191b29742'
-
-        # Open the login page
-        driver.get(login_url)
-
-        # Find the username and password input fields and submit button
-        username_field = driver.find_element(By.ID, 'ap_email')
-        password_field = driver.find_element(By.ID, 'ap_password')
-        submit_button = driver.find_element(By.ID, 'signInSubmit')
-
-        # Input your username and password
-        username_field.send_keys(os.getenv("EMAIL"))
-        password_field.send_keys(os.getenv("PASSWORD"))
-
-        # Click the submit button
-        submit_button.click()
-
-        # Wait for the login to complete (you might need to adjust the waiting time)
-        time.sleep(5)
-
-        # Return the driver after login
-        return driver
 
     def get_parsed_html(self):
         driver = webdriver.Chrome()
-        self.login_to_goodreads(driver)
-        url = f'https://www.goodreads.com/review/list/{self.id}?shelf=read&utf8=%E2%9C%93&per_page=100e&read_at={self.year}&view=table'        
+        url = f'https://www.goodreads.com/review/list/{self.id}?shelf=read&utf8=%E2%9C%93&per_page=infinite&read_at={self.year}&view=table'        
         driver.get(url)
-
         try:
             # Set a maximum number of scrolls to avoid an infinite loop
-            max_scrolls = 10
-            current_scroll = 0
-
-            while current_scroll < max_scrolls:
+            while not self.areAllBooksLoaded(driver):
                 # Scroll to the bottom of the page
-                driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-
-                # Wait for a short period to allow content to load
-                time.sleep(2)  # Adjust as needed
-
-                # Check if the target element is present
-                target_element_present = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'responsiveSiteFooter'))
-                )
-
-                if target_element_present:
-                    break
-
-                current_scroll += 1
+             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+            # Wait for the page to load after scrolling
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'infiniteStatus')))
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
@@ -77,6 +35,13 @@ class BookDetails():
         driver.quit()
         self.fetch_book_details(soup)
         return self.store_book_details
+
+    def areAllBooksLoaded(self,driver):
+        infinite_status_element = driver.find_element(By.ID, 'infiniteStatus')
+        # Extract numbers from the text and check if they are equal
+        numbers = [int(s) for s in infinite_status_element.text.split() if s.isdigit()]
+        return numbers[0] == numbers[1]
+
     
     def fetch_book_details(self, soup):
         username= (soup.find("title").text.split(chr(8217)))[0]
@@ -105,7 +70,7 @@ class BookDetails():
             obj_name = { 'title': title,
             'author': author,
             'page': page,
-            'rating': rating,
+            #'rating': rating,
             'avgrating': avgrating,
             'booklink': booklink,
             'bookcover': bookcover,
