aa4a8e219f0d4364021ac585b0ce84aaed6591a0
Arushi <aggarwalarushi5@gmail.com>
2024-01-16 23:30:48 +0530

    wip - working on async for server tasks

--- server/app.py
+++ server/app.py
@@ -1,18 +1,19 @@
 from fetch_book_details import BookDetails
 from flask import Flask, request
 from flask_cors import CORS
+import asyncio
 
 app = Flask(__name__) 
 
 CORS(app, resources={r'/*' : {'origins': '*'}})
 
 @app.route('/wrapped', methods=['GET'])
-def fetch_goodreads_data():
+async def fetch_goodreads_data():
     id = request.args.get('gr_user_id')
     year= request.args.get('year')
     bd = BookDetails(19117004, 2023)
-    book_details_data = bd.get_parsed_html()
-    return book_details_data
+    return await bd.get_parsed_html()
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- server/fetch_book_details.py
+++ server/fetch_book_details.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import asyncio
 
 class BookDetails():
     def __init__(self, userid, year):
@@ -16,7 +16,7 @@ class BookDetails():
         self.store_book_details = {'totalbooksread': 0, 'totalpagesread': 0 , 'username': '', 'books': []}
         self.rows = []
 
-    def get_parsed_html(self):
+    async def get_parsed_html(self):
         driver = webdriver.Chrome()
         url = f'https://www.goodreads.com/review/list/{self.id}?shelf=read&utf8=%E2%9C%93&per_page=infinite&read_at={self.year}&view=table'        
         driver.get(url)
@@ -24,7 +24,7 @@ class BookDetails():
             # Set a maximum number of scrolls to avoid an infinite loop
             while not self.areAllBooksLoaded(driver):
                 # Scroll to the bottom of the page
-             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     
             # Wait for the page to load after scrolling
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'infiniteStatus')))
@@ -33,7 +33,7 @@ class BookDetails():
 
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         driver.quit()
-        self.fetch_book_details(soup)
+        await self.process_read_books(soup)
         return self.store_book_details
 
     def areAllBooksLoaded(self,driver):
@@ -41,9 +41,8 @@ class BookDetails():
         # Extract numbers from the text and check if they are equal
         numbers = [int(s) for s in infinite_status_element.text.split() if s.isdigit()]
         return numbers[0] == numbers[1]
-
     
-    def fetch_book_details(self, soup):
+    async def process_read_books(self, soup):
         username= (soup.find("title").text.split(chr(8217)))[0]
         self.store_book_details['username'] = username
         date_read_rows = []
@@ -53,38 +52,43 @@ class BookDetails():
             date_read_rows.append(element.find_parent('tr'))
         #parsing over year()
         self.store_book_details['totalbooksread'] += len(date_read_rows)
+        tasks = []
         for i, book_rows in enumerate(date_read_rows):
             self.rows = book_rows
-            obj_name = str(i)
-            title=self.format_element('title', tag='a')
-            author= self.format_element('author',tag='a')
-            readcount = self.format_element('read_count')
-            page = int(re.findall(r'\d+', self.format_element('num_pages'))[0])
-            rating= self.map_rating()            
-            avgrating = float(self.format_element('avg_rating'))
-            booklink = 'https://www.goodreads.com' + book_rows.find_next('td', class_=f'field cover').find('a')['href']
-            bookcover_comp = book_rows.find_next('td', class_=f'field cover').find('img')['src']
-            bookcover = re.sub(r'\.(?:_SY|_SX)\d+_', '', bookcover_comp)
-            #genre = self.get_genres(booklink)
-            #create book object
+            tasks.append(self.fetch_book_details(book_rows, i))
+        await asyncio.gather(*tasks)
+    
+    async def fetch_book_details(self, book_rows, i):
+        obj_name = str(i)
+        title=self.format_element('title', tag='a')
+        author= self.format_element('author',tag='a')
+        readcount = self.format_element('read_count')
+        page = int(re.findall(r'\d+', self.format_element('num_pages'))[0])
+        rating= self.map_rating()            
+        avgrating = float(self.format_element('avg_rating'))
+        booklink = 'https://www.goodreads.com' + book_rows.find_next('td', class_=f'field cover').find('a')['href']
+        bookcover_comp = book_rows.find_next('td', class_=f'field cover').find('img')['src']
+        bookcover = re.sub(r'\.(?:_SY|_SX)\d+_', '', bookcover_comp)
+        genre = await self.get_genres(booklink)
+        #create book object
 
-            obj_name = {
-            'title': title,
-            'author': author,
-            'page': page,
-            'rating': rating,
-            'avgrating': avgrating,
-            'booklink': booklink,
-            'bookcover': bookcover,
-            #'genre' : genre,
-            'readcount' : readcount
-            }
-            self.store_book_details['books'].append(obj_name)
-            self.store_book_details['totalpagesread'] += int(page)
+        obj_name = {
+        'title': title,
+        'author': author,
+        'page': page,
+        'rating': rating,
+        'avgrating': avgrating,
+        'booklink': booklink,
+        'bookcover': bookcover,
+        'genre' : genre,
+        'readcount' : readcount
+        }
+        self.store_book_details['books'].append(obj_name)
+        self.store_book_details['totalpagesread'] += int(page)
 
-    def get_genres(self, booklink):
+    async def get_genres(self, booklink):
         forbidden_genres = ['Fiction', 'Nonfiction', 'Short Stories', 'Anthology', 'Adult']
-        res = requests.get(booklink)
+        res = await requests.get(booklink)
         if (res.status_code == 200):
             intended_class = 'BookPageMetadataSection__genres'
             parse_only= SoupStrainer(class_= intended_class)
@@ -93,6 +97,12 @@ class BookDetails():
             for genre in genres:
                 if genre not in forbidden_genres:
                     return genre
+                
+    async def parse_book_info_html(self, url):
+        loop = asyncio.get_event_loop()
+        future = loop.run_in_executor(None, requests.get, url)
+        return await future
+    
     def update_count(self, key,value, inc = 1):
         self.store_book_details.setdefault((key), {})
         self.store_book_details[key][value] = self.store_book_details[key].get(value, 0) + inc
