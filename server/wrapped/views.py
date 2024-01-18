6540bfb8d77487d11d8e509083fb8848c4794de6
Arushi <aggarwalarushi5@gmail.com>
2024-01-18 22:12:30 +0530

    wip - add http methods to views.py

--- server/wrapped/views.py
+++ server/wrapped/views.py
@@ -1,17 +1,19 @@
-from urllib import request
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_http_methods
 from fetch_book_details import BookDetails
 from validate_users import Users
 
-async def fetch_goodreads_data():
-    id = request.args.get('uid')
-    year= request.args.get('year')
+@require_http_methods(["GET"])
+async def fetch_goodreads_data(request: HttpResponse):
+    id = request.GET['uid']
+    year= request.GET['year']
     bd = BookDetails(19117004, 2022)
     return await JsonResponse(bd.get_parsed_html())
 
-def validate_users():
-    id = request.args.get('uid')
+@require_http_methods(["GET"])
+def validate_users(request: HttpResponse):
+    id = request.GET['uid']
     users = Users(id)
     return JsonResponse(users.validate())
 # Create your views here.
