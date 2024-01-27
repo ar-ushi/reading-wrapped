bb8ce48804dc9f24daee82de9b1e6f93765285b4
Arushi <aggarwalarushi5@gmail.com>
2024-01-28 00:22:21 +0530

    wip - handle read and write to db

--- server/wrapped/models.py
+++ server/wrapped/models.py
@@ -2,13 +2,10 @@ from django.db import models
 from django.forms import JSONField
 
 # Create your models here.
-class Year(models.Model):
-    year = models.PositiveIntegerField(unique= True)
-
 class BookData(models.Model):
-    userid = models.PositiveIntegerField()
-    year = models.ForeignKey(Year, on_delete= models.CASCADE)
-    reading_wrapped = JSONField()
+    uid = models.PositiveIntegerField()
+    year = models.PositiveIntegerField(unique=True)
+    yearlystats = JSONField()
 
     class Meta:
         unique_together = ('userid', 'year')
--- server/wrapped/views.py
+++ server/wrapped/views.py
@@ -1,17 +1,26 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import require_http_methods
 from wrapped.fetch_book_details import BookDetails
 from wrapped.validate_users import Users
+from .models import BookData
 
 @require_http_methods(["GET"])
 async def fetch_goodreads_data(request: HttpRequest):
     id = request.GET['uid']
     year= request.GET['year']
-    
-    bd = BookDetails(19117004, 2022)
-    readingstats = await bd.get_parsed_html()
-    return JsonResponse(readingstats)
+    try:
+        book_data = BookData.objets.get(uid=id, year=year)
+        return JsonResponse(book_data.yearlystats)
+    except BookData.DoesNotExist: 
+        bd = BookDetails(id, year)
+        readingstats = await bd.get_parsed_html()
+        try:
+            BookData.objects.create(uid=id, year=year, yearlystats = readingstats)
+        except IntegrityError:
+            pass
+        return JsonResponse(readingstats)
 
 @require_http_methods(["GET"])
 def validate_users(request: HttpRequest):
